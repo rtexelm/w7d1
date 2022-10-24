25bc6710424a23f0cf9f366ec32c985f1b79633b
rtexelm <texelist@gmail.com>
2022-10-24 15:05:35 -0400

    Completed user model

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -0,0 +1,57 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string
+#  password_digest :string
+#  session_token   :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+
+  before_validation :ensure_session_token
+
+  validates :username, :session_token, :password_digest, presence: true
+  validates :username, :session_token, uniqueness: true
+
+  validates :password, length { minimum: 6 }, allow_nil: true
+
+  attr_reader :password
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password) # Create hash and salt
+    @password = password # Create isntance for validation
+  end
+
+  def is_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(:username = username)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  
+  def reset_session_token
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+  
+  private
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+  
+  def generate_unique_session_token
+    SecureRandom::urlsafe_base64
+  end
+end
